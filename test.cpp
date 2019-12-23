4cfd21b113df8ef0ed1e875fc7f4b2247c782e8a
chris <cngumbi35@gmail.com>
2019-12-23 15:02:44 +0300

    new commit of the test program

---
+++ test.cpp
@@ -0,0 +1,13 @@
+//to test the uses of some operaters
+//# is the same as %:
+//[] is the same as <::>
+//{} is the same as <%%>
+// now lets see the 
+%:include <stdio.h>
+
+int main (int argc, char *argv<::>)<%
+
+printf("the man in the white shirt");
+return 0;
+
+%>
