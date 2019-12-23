db9cfda66745379a215a6d1523bdcc505bdfb4e5
chris <cngumbi35@gmail.com>
2019-12-23 02:10:44 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,47 @@
+#include "tac.h"
+
+int main (int argc char *argv[]){
+	//ply is player 
+	//chc is choice
+	//mrk is mark
+	int ply = 1, i , chc;
+	char mrk;
+	//create a loop using do
+	do
+	{
+		brd();
+		ply = (ply % 2)? 1 : 2;
+		printf("Player  %d  , enter a number:  ",ply);
+		scanf("%d", &chc);
+		mrk = (ply == 1) ? 'X' : 'O';
+		if (chc == 1 && sqr[1] == '1')
+			sqr[1] = mrk;
+		else if (chc == 2 && sqr[2] =='2')
+			sqr[2] = mrk;
+		else if (chc == 3 && sqr[3] =='3')
+			sqr[3] = mrk;
+		else if (chc == 4 && sqr[4] =='4')
+			sqr[4] = mrk;
+		else if (chc == 5 && sqr[5] =='5')
+			sqr[5] = mrk;
+		else if(chc == 6 && sqr[6] =='6')
+			sqr[6] = mrk;
+		else if(chc == 7 && sqr[7] =='7')
+			sqr[7] = mrk;
+		else if(chc == 8 && sqr[8] =='8')
+			sqr[8] = mrk;
+		else if(chc == 9 && sqr[9] =='9')
+			sqr[9] = mrk;
+		else
+		{
+			printf("Invalid move\n");
+			ply--;
+		}
+		i = chckwn();
+		ply++;
+	}while(i == -1);
+	brd();
+	if(i == 1)
+		printf("==>\a Player %d wins\n",ply);
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,22 @@
+#the makefile of tic tac toe
+# define constants CC CFLAGS
+# create macros  DEPS which is the set of .h files which .c files depend on.
+# create constant OBJS for object files
+# $@ left side of :
+# $^ right side of :
+CC=gcc
+CFLAGS=-std=c11
+DEPS=tac.h
+OBJS=main.o tac.o
+
+%.o: %.c $(DEPS)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+tac: $(OBJS)
+	$(CC) -o $@ $^ $(CFLAGS)
+
+.PHONY:clean
+
+
+clean:
+	rm -f $(OBJS)
