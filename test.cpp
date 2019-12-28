06e35c8b300041fac272ba3fa2ed5d63bdf45eeb
cngumbi35 <cngumbi35@gmail.com>
2019-12-29 00:36:47 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,10 +4,20 @@
 //{} is the same as <%%>
 // now lets see the 
 %:include <stdio.h>
-
+int tsum(int k, int j);
 int main (int argc, char *argv<::>)<%
-
+int a, d;
 printf("the man in the white shirt");
+printf("the representation of the used");
+printf("enter valeu 1");
+scanf("%d",)
+printf("enter valeu 2");
+scanf("%d", d);
+printf("sum is\t",tsum(a,b);
 return 0;
 
+%>
+int tsum(int k, int j);<
+% sum = k + j;
+return sum;
 %>
