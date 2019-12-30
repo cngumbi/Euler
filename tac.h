3e74bd464d98dadffe27d0e2ada50cd83a27ca34
cngumbi35 <cngumbi35@gmail.com>
2019-12-30 14:43:29 +0300

    Update tac.h

--- tac.h
+++ tac.h
@@ -2,7 +2,7 @@
 #define TAC_H
 
 #include <stdio.h>
-//#include <conio.h>
+#include <conio.h>
 char sqr[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 //function to chech win
 //chckwn is check win
