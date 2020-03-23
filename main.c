23f86744cadaf3b69b4142071809b2296f65bd52
cngumbi <cngumbi35@gmail.com>
2020-03-23 19:06:34 +0300

    new commit

--- main.c
+++ main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
-#define UPPER 300
-#define STEP  20
-#define LOWER 0
 int main()
 {
-	float fahr;
 
-	printf("FAHRENHEITS\t CELSIUS\n");
-	for(fahr = UPPER; fahr >= LOWER; fahr -= STEP)
-	{
-		printf("%3.2f\t\t%6.2f\n",fahr,(5.0/9.0)*(fahr - 32));
-
-	}
 }
