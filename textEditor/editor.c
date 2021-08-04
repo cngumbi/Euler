5abc4b1e8b6e522cc92e0380d396d7abd52f5f3b
cngumbi <cngumbi35@gmail.com>
2021-08-04 16:03:35 +0300

    added new files to handle header

--- textEditor/editor.c
+++ textEditor/editor.c
@@ -3,62 +3,6 @@
 //author: cngumbi
 //version: 1.0.0
 //
-//*******************INCLUDES************************
-//
-//feature test macro
-//
-#define _DEFAULT_SOURCE
-#define _BSD_SOURCE
-#define _GNU_SOURCE
-
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <time.h>
-#include <unistd.h>
-//
-//*******************DEFINE****************************
-//
-#define EDITOR_VERSION "0.0.1"
-#define EDITOR_TAB_STOP 8
-#define EDITOR_QUIT_TIMES 3
-
-#define CTRL_KEY(k) ((k) & 0x1f)
-
-//create an enum to store the arrow keys
-//
-enum editorKey{
-	BACKSPACE = 127,
-	ARROW_LEFT = 1000,
-	ARROW_RIGHT,
-	ARROW_UP,
-	ARROW_DOWN,
-	DEL_KEY,
-	HOME_KEY,
-	END_KEY,
-	PAGE_UP,
-	PAGE_DOWN
-};
-enum editorHighlight{
-	HL_NORMAL = 0,
-	HL_COMMENT,
-	HL_MLCOMMENT,
-	HL_KEYWORD1,
-	HL_KEYWORD2,
-	HL_STRING,
-	HL_NUMBER,
-	HL_MATCH
-};
-
-#define HL_HIGHLIGHT_NUMBERS (1<<0)
-#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 //
 //*******************DATA*******************************
