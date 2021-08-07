6eb81b2f8324efb8a2110e192985b15a1a1f52b0
cngumbi <cngumbi35@gmail.com>
2021-08-07 13:36:58 +0300

    made changes on the editor file

--- textEditor/editor.c
+++ textEditor/editor.c
@@ -3,50 +3,7 @@
 //author: cngumbi
 //version: 1.0.0
 //
-
-//
-//*******************DATA*******************************
-//
-//
-//this is a data type for storing a row of text in our editor
-//
-struct editorSyntax{
-	char *filetype;
-	char **filematch;
-	char **keywords;
-	char *singleline_comment_start;
-	char *multiline_comment_start;
-	char *multiline_comment_end;
-	int flags;
-};
-typedef struct erow{
-	int idx;
-	int size;
-	int rsize;
-	char *chars;
-	char *render;
-	unsigned char *hl; // hl stands for highlight
-	int hl_open_comment;
-
-}erow;
-
-struct editorConfig{
-	int vx, vy;
-	int rx;
-	int rowoff;
-	int coloff;
-	int screenrows;
-	int screencols;
-	int numrows;
-	erow *row;
-	int dirty;
-	char *filename;
-	char statusmsg[80];
-	time_t statusmsg_time;
-	struct editorSyntax *syntax;
-	struct termios orig_termios;
-};
-struct editorConfig K;
+#include "stype.h"
 //
 //*********************FILETYPES**************************
 //
